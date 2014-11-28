068f88ff177380637f1eb452829b723f8d8be6d9
Manasvi Saxena <msaxena2@illinois.edu>
2014-11-28 09:50:26 -0500

    Completed Result Class, To be modified for substitution

--- kernel/src/main/java/org/kframework/krun/api/KRunDebuggerResult.java
+++ kernel/src/main/java/org/kframework/krun/api/KRunDebuggerResult.java
@@ -4,8 +4,46 @@ package org.kframework.krun.api;
 
 /**
  * Contains information about a debugger step operation.
- * Mainly Contains information about the substitution.
  */
 public class KRunDebuggerResult {
 
+    private KRunState originalState;
+    private KRunState steppedState;
+    private Transition rule;
+
+    public KRunDebuggerResult() {
+        originalState = null;
+        steppedState = null;
+        rule = null;
+    }
+
+    public KRunDebuggerResult(KRunState originalState, KRunState steppedState, Transition rule) {
+        this.originalState = originalState;
+        this.steppedState = steppedState;
+        this.rule = rule;
+    }
+
+    public KRunState getOriginalState() {
+        return originalState;
+    }
+
+    public void setOriginalState(KRunState originalState) {
+        this.originalState = originalState;
+    }
+
+    public KRunState getSteppedState() {
+        return steppedState;
+    }
+
+    public void setSteppedState(KRunState steppedState) {
+        this.steppedState = steppedState;
+    }
+
+    public Transition getRule() {
+        return rule;
+    }
+
+    public void setRule(Transition rule) {
+        this.rule = rule;
+    }
 }
