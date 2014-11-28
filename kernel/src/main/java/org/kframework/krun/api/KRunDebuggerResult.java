73cf41b8e5366381232d37da44c950957ad14929
Manasvi Saxena <msaxena2@illinois.edu>
2014-11-28 09:23:44 -0500

    New Result file for debugger related data

---
+++ kernel/src/main/java/org/kframework/krun/api/KRunDebuggerResult.java
@@ -0,0 +1,11 @@
+// Copyright (c) 2013-2014 K Team. All Rights Reserved.
+
+package org.kframework.krun.api;
+
+/**
+ * Contains information about a debugger step operation.
+ * Mainly Contains information about the substitution.
+ */
+public class KRunDebuggerResult {
+
+}
