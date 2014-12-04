b03a152373a9fcca19d76681bd86d1b1fd313206
Manasvi Saxena <msaxena2@illinois.edu>
2014-12-04 10:53:11 -0600

    Added the debug graph

---
+++ kernel/src/main/java/org/kframework/krun/api/KRunDebugGraph.java
@@ -0,0 +1,11 @@
+// Copyright (c) 2014 K Team. All Rights Reserved.
+
+package org.kframework.krun.api;
+
+import edu.uci.ics.jung.graph.DirectedOrderedSparseMultigraph;
+
+/**
+ * Graph to represent the debugger's execution state.
+ */
+public class KRunDebugGraph extends DirectedOrderedSparseMultigraph<KRunState, DebugTransition> {
+}
