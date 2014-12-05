bea4ff441d2a07d43768fa6c5207d831f0f42c86
Manasvi Saxena <msaxena2@illinois.edu>
2014-12-04 20:03:01 -0600

    modifications, change to add a supporting structure

--- kernel/src/main/java/org/kframework/krun/api/ExecutorDebugger.java
+++ kernel/src/main/java/org/kframework/krun/api/ExecutorDebugger.java
@@ -1,6 +1,5 @@
 // Copyright (c) 2013-2014 K Team. All Rights Reserved.
 package org.kframework.krun.api;
-
 import org.apache.commons.collections4.BidiMap;
 import org.apache.commons.collections4.bidimap.DualHashBidiMap;
 import org.kframework.compile.utils.CompilerStepDone;
@@ -23,28 +22,21 @@ import org.kframework.krun.tools.Debugger;
 import org.kframework.krun.tools.Executor;
 import org.kframework.parser.TermLoader;
 import org.kframework.utils.errorsystem.KExceptionManager;
-
 import com.google.inject.Inject;
-
 import edu.uci.ics.jung.graph.DirectedGraph;
 import edu.uci.ics.jung.graph.util.Pair;
-
 import java.util.Map.Entry;
-
 public class ExecutorDebugger implements Debugger {
     private Integer currentState;
     private KRunGraph graph;
     private BidiMap<Integer, KRunState> states;
-
     private static Rule defaultPattern;
     private static RuleCompilerSteps defaultPatternInfo;
-
     private final Context context;
     private final Executor executor;
     private final TermLoader loader;
     private final KExceptionManager kem;
     private final KRunState.Counter counter;
-
     @Inject
     public ExecutorDebugger(
             Executor executor,
@@ -58,7 +50,6 @@ public class ExecutorDebugger implements Debugger {
         this.kem = kem;
         this.counter = counter;
     }
-
     @Override
     public void start(Term initialConfiguration) throws KRunExecutionException {
         try {
@@ -69,12 +60,10 @@ public class ExecutorDebugger implements Debugger {
                     context);
             defaultPatternInfo = new RuleCompilerSteps(context, kem);
             pattern = defaultPatternInfo.compile(new Rule((Sentence) pattern), null);
-
             defaultPattern = (Rule) pattern;
         } catch (CompilerStepDone e) {
             e.printStackTrace();
         }
-
         KRunState initialState = new KRunState(initialConfiguration, counter);
         graph = new KRunGraph();
         graph.addVertex(initialState);
@@ -91,7 +80,6 @@ public class ExecutorDebugger implements Debugger {
             currentState = initialState.getStateId();
         }
     }
-
     /**
      * Adds the new state to the states map
      * @param state new state to add
@@ -104,15 +92,12 @@ public class ExecutorDebugger implements Debugger {
         }
         return false;
     }
-
     public KRunGraph getGraph() {
         return graph;
     }
-
     public Integer getCurrentState() {
         return currentState;
     }
-
     public void setCurrentState(Integer stateNum) throws IllegalArgumentException {
         if (stateNum == null || states.containsKey(stateNum)) {
             currentState = stateNum;
@@ -121,13 +106,11 @@ public class ExecutorDebugger implements Debugger {
                     + ": it does not exist in the graph.");
         }
     }
-
     public KRunState getState(int stateNum) {
         KRunState state = states.get(stateNum);
         if (state == null) throw new IllegalArgumentException("State " + stateNum + " does not exist in the graph.");
         return state;
     }
-
     private void steppingLoop(Integer steps) throws KRunExecutionException {
         if (currentState == null) {
             throw new IllegalStateException("Cannot step without a current state to step from. "
@@ -139,10 +122,9 @@ public class ExecutorDebugger implements Debugger {
             Entry<Integer, KRunState> prevValue = containsValue(nextStep);
             if (prevValue!=null) {
                 nextStep = prevValue.getValue();
-
                 int stateId = prevValue.getKey();
                 if (stateId == currentState) {
-                    //we've stopped moving, so that means we must have reached a final state
+                //we've stopped moving, so that means we must have reached a final state
                     return;
                 }
                 // we've reached this state before, so update the current state and proceed to the next step
@@ -157,15 +139,12 @@ public class ExecutorDebugger implements Debugger {
             currentState = nextStep.getStateId();
         }
     }
-
     public void step(int steps) throws KRunExecutionException {
         steppingLoop(steps);
     }
-
     public void resume() throws KRunExecutionException {
         steppingLoop(null);
     }
-
     public SearchResults stepAll(int steps) throws KRunExecutionException {
         if (currentState == null) {
             throw new IllegalStateException("Cannot step without a current state to step from. "
@@ -177,7 +156,6 @@ public class ExecutorDebugger implements Debugger {
         currentState = null;
         return results;
     }
-
     private void mergeSearchGraph(DirectedGraph<KRunState, Transition> graphFragment) {
         for (KRunState state : graphFragment.getVertices()) {
             //check if graph already contains state
@@ -209,11 +187,10 @@ public class ExecutorDebugger implements Debugger {
             }
         }
     }
-
     /* checks if state already exists(using Semantic equal)
-     * if it exists return old value
-     * this intends to replace states.containsValue which uses hash and equals defined in KRunState
-     */
+    * if it exists return old value
+    * this intends to replace states.containsValue which uses hash and equals defined in KRunState
+    */
     private Entry<Integer, KRunState> containsValue(KRunState state){
         for (Entry<Integer,KRunState> e : states.entrySet() ){
             if(SemanticEqual.checkEquality(state.getRawResult(), e.getValue().getRawResult()))
@@ -225,7 +202,6 @@ public class ExecutorDebugger implements Debugger {
         int stateNum = states.getKey(state);
         return states.get(stateNum);
     }
-
     public Transition getEdge(int state1, int state2) {
         KRunState first = getState(state1);
         KRunState second = getState(state2);
@@ -235,7 +211,6 @@ public class ExecutorDebugger implements Debugger {
                     + state1 + " and " + state2 + " does not exist in the current graph");
         return edge;
     }
-
     public void readFromStdin(String s) {
         if (currentState == null) {
             throw new IllegalStateException("Cannot read from stdin without a current state to step from. "
@@ -248,7 +223,7 @@ public class ExecutorDebugger implements Debugger {
         result = (Term) transformer.visitNode(configuration);
         if (!transformer.getSucceeded()) {
             throw new IllegalStateException("Cannot perform command: Configuration does not " +
-                "have an stdin buffer");
+                    "have an stdin buffer");
         }
         KRunState newState = new KRunState(result, counter);
         Entry<Integer, KRunState> prevValue = containsValue(newState);
@@ -257,7 +232,7 @@ public class ExecutorDebugger implements Debugger {
             Transition edge = graph.findEdge(getState(currentState), canonicalNewState);
             if (edge == null) {
                 graph.addEdge(Transition.stdin(s),
-                    getState(currentState), canonicalNewState);
+                        getState(currentState), canonicalNewState);
             }
             currentState = canonicalNewState.getStateId();
             return;
@@ -265,10 +240,9 @@ public class ExecutorDebugger implements Debugger {
         putState(newState);
         graph.addVertex(newState);
         graph.addEdge(Transition.stdin(s),
-            getState(currentState), newState);
+                getState(currentState), newState);
         currentState = newState.getStateId();
     }
-
     private static class AppendToStdin extends CopyOnWriteTransformer {
         private String str;
         private boolean succeeded;
@@ -280,44 +254,39 @@ public class ExecutorDebugger implements Debugger {
             inStdin = false;
             inBuffer = false;
         }
-
         public boolean getSucceeded() {
             return succeeded;
         }
-
         @Override
-        public ASTNode visit(Cell cell, Void _)  {
+        public ASTNode visit(Cell cell, Void _void) {
             if ("stdin".equals(context.cells.get(cell.getLabel())
-                .getCellAttributes().get("stream"))) {
+                    .getCellAttributes().get("stream"))) {
                 inStdin = true;
-                ASTNode result = super.visit(cell, _);
+                ASTNode result = super.visit(cell, _void);
                 inStdin = false;
                 return result;
             }
-            return super.visit(cell, _);
+            return super.visit(cell, _void);
         }
-
         @Override
-        public ASTNode visit(KApp kapp, Void _)  {
+        public ASTNode visit(KApp kapp, Void _void) {
             if (kapp.getLabel().equals(KLabelConstant.of("'#buffer"))) {
                 inBuffer = true;
-                ASTNode result = super.visit(kapp, _);
+                ASTNode result = super.visit(kapp, _void);
                 inBuffer = false;
                 return result;
             }
-            return super.visit(kapp, _);
+            return super.visit(kapp, _void);
         }
-
         @Override
-        public ASTNode visit(StringBuiltin s, Void _)  {
+        public ASTNode visit(StringBuiltin s, Void _void) {
             if (inStdin && inBuffer) {
                 succeeded = true;
                 return StringBuiltin.of(s.stringValue() + str);
             }
-            return super.visit(s, _);
+            return super.visit(s, _void);
         }
     }
-
     @Override
     public void setGraph(KRunGraph graph) {
         this.graph = graph;
