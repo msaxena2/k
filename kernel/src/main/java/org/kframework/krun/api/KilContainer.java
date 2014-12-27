b4ff3101f177f6c763cd1b226f35a1686973211a
Manasvi <msaxena2@illinois.edu>
2014-12-27 18:03:15 +0530

    added the new container, allowing for lazy evaluation transformation

---
+++ kernel/src/main/java/org/kframework/krun/api/KilContainer.java
@@ -0,0 +1,33 @@
+// Copyright (c) 2014 K Team. All Rights Reserved.
+
+package org.kframework.krun.api;
+
+import org.kframework.kil.Context;
+import org.kframework.kil.Term;
+
+/**
+ * Abstract class, to be extended by every container specific to the backend.
+ * Allows for lazy evaluation of a backend specific term to a generic kernel term.
+ */
+public abstract class KilContainer {
+
+    private Term kilTerm;
+
+    private TermType type;
+
+    /**
+     * To be implemented in the extending class.
+     * When called, the function should return the kilTerm, if already calculated,
+     * or calculate it using the context and the backend term, and then return it.
+     */
+
+    public abstract Term getKilTerm(Context context);
+
+    public TermType getType() {
+        return type;
+    }
+
+    public void setType(TermType type) {
+        this.type = type;
+    }
+}
---
+++ kernel/src/main/java/org/kframework/krun/api/TermType.java
@@ -0,0 +1,8 @@
+// Copyright (c) 2014 K Team. All Rights Reserved.
+
+package org.kframework.krun.api;
+
+
+public enum TermType {
+    STATE, RULE, UNDEFINED
+}
