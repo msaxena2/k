29b42b0cbc233a1ae0550f8cdf480bd324148120
Manasvi <msaxena2@illinois.edu>
2014-12-29 05:51:43 +0530

    added the new class for the step result, and the search result as well

---
+++ kernel/src/main/java/org/kframework/krun/api/RewriteRelation.java
@@ -0,0 +1,31 @@
+// Copyright (c) 2014 K Team. All Rights Reserved.
+
+package org.kframework.krun.api;
+
+
+import java.util.Optional;
+
+/**
+ * The result of a step operation in the debugger.
+ * Contains the step/search graph if the compute graph option is specified.
+ */
+public class RewriteRelation {
+    public KRunStateUnit finalState;
+    Optional<KRunExecutionGraph> executionGraph;
+
+    public KRunStateUnit getFinalState() {
+        return finalState;
+    }
+
+    public void setFinalState(KRunStateUnit finalState) {
+        this.finalState = finalState;
+    }
+
+    public Optional<KRunExecutionGraph> getExecutionGraph() {
+        return executionGraph;
+    }
+
+    public void setExecutionGraph(Optional<KRunExecutionGraph> executionGraph) {
+        this.executionGraph = executionGraph;
+    }
+}
