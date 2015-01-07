ca6b4b4d51521a788ca973052128c41a4978c516
Manasvi <msaxena2@illinois.edu>
2015-01-07 13:51:36 +0530

    added another class for rules

---
+++ kernel/src/main/java/org/kframework/krun/api/io/KilRuleContainer.java
@@ -0,0 +1,33 @@
+package org.kframework.krun.api.io;
+
+import org.kframework.kil.Context;
+import org.kframework.kil.Rule;
+
+import java.util.Optional;
+
+/**
+ * Container for Generic Kil Rules.
+ */
+public abstract class KilRuleContainer {
+    private Optional<Rule> rule;
+
+    protected Context context;
+
+    public KilRuleContainer(Rule rule, Context context) {
+        this.rule = Optional.ofNullable(rule);
+        this.context = context;
+    }
+
+
+    /**
+     * To be implemented in the extending class.
+     * When called, the function should return the kilRule, if already calculated,
+     * or calculate it using the context and the backend Rule, and then return it.
+     */
+
+    public abstract Rule getKilTerm();
+
+    public abstract boolean equals(Object obj);
+
+    public abstract int hashCode();
+}
